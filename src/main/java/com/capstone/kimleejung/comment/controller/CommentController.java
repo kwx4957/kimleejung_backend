7ec2ce2920bcf018c513858ff3658b10cd8b1f90
kwx4957 <kwx4957@gmail.com>
2022-09-19 13:53:09 +0900

    OpenAPI 3 추가

--- src/main/java/com/capstone/kimleejung/comment/controller/CommentController.java
+++ src/main/java/com/capstone/kimleejung/comment/controller/CommentController.java
@@ -4,12 +4,9 @@ import com.capstone.kimleejung.comment.model.CommentDto;
 import com.capstone.kimleejung.comment.repository.CommentRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class CommentController {
 
     private final CommentRepository commentRepository;
