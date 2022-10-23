8806edcf0408a28d46dc053634b5329a58464320
kwx4957 <kwx4957@gmail.com>
2022-10-23 13:13:30 +0900

    1. 기존 로그인 클래스 삭제 2. 공공데이터 Entity 생성

--- src/main/java/com/capstone/kimleejung/enterprise/entity/StockDivideInfo.java
+++ src/main/java/com/capstone/kimleejung/enterprise/entity/StockDivideInfo.java
@@ -10,8 +10,25 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 public class StockDivideInfo {
+    // https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15043284
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    private int basDt;
+    private int crno;
+    private String stckIssuCmpyNm;
+    private int dvdnBasDt;
+    private int cashDvdnPayDt;
+    private int stckHndvDt;
+    private int stckDvdnRcd;
+    private String stckDvdnRcdNm;
+    private int stckGenrDvdnAmt;
+    private int stckGrdnDvdnAmt;
+    private int stckGenrCashDvdnRt;
+    private int stckGenrDvdnRt;
+    private int cashGrdnDvdnRt;
+    private int stckGrdnDvdnRt;
+    private int stckParPrc;
+    private int stckStacMd;
 
 }
--- src/test/java/com/capstone/kimleejung/comment/controller/CommentControllerTest.java
+++ src/test/java/com/capstone/kimleejung/comment/controller/CommentControllerTest.java
@@ -1,7 +1,7 @@
 package com.capstone.kimleejung.comment.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class CommentControllerTest {
+
 
 }
